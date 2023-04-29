def60b1be3208337cb52f2640b506a3ae97a3483
saluvalasaichandu <saluvalasaichand@gmail.com>
2023-04-29 20:30:50 +0530

    DeeperConcepts_React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React,{useState}from 'react';
 import './App.css';
+import Button from './components/Button/Button';
 
 function App() {
+  const[showParagraph,setShowParagraph]=useState(false);
+  console.log("App is Running");
+  const toggleParagraphHandler=()=>{
+    setShowParagraph(!showParagraph)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Hi there!</h1>
+      <h2>React deeper concepts</h2>
+      {showParagraph&&<p>This is New paragragh</p>}
+      <Button onClick={toggleParagraphHandler}>Show Toggled Praragraph</Button>
     </div>
   );
 }
